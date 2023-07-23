2992a24f7e39aece574c88781fe8f180f1c8f2ed
berwyn <berwyn@pop-os.localdomain>
2023-07-23 23:18:00 +0800

    initial for vue authentication

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import store from "./store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from './plugins/font-awesome'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+app.component("font-awesome-icon", FontAwesomeIcon)
+
+app.mount('#app')
